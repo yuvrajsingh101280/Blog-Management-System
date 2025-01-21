1e9e8095079a0c7cd186d1d5bb0447b611c9c434
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-01-21 16:04:13 +0530

    Remove blog controller and routes; update user model to include password and unique email

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 import dotenv from "dotenv";
-import blogroute from "../routes/blog";
 dotenv.config();
 const prisma = new PrismaClient();
 const app = express();
@@ -9,8 +8,6 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 
 // /router
-
-app.use("/api/", blogroute);
 
 app.listen(port, () => {
   console.log(`server starting at http://localhost:${port}`);
