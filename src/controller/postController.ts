ff294f3775132a2f39ce0054e66ac0550cc9835a
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-01-22 21:14:38 +0530

    Add CRUD operations for posts and secure routes with token verification

---
+++ src/controller/postController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+export const createPost = async (req: Request, res: Response) => {};
+export const getAllPost = async (req: Request, res: Response) => {};
+export const getPost = async (req: Request, res: Response) => {};
+export const editpost = async (req: Request, res: Response) => {};
+
+export const deletePost = async (req: Request, res: Response) => {};
--- src/routes/PostRoutes.ts
+++ src/routes/PostRoutes.ts
@@ -1,4 +1,18 @@
 import express from "express";
+import verifyToken from "../middleware/authMiddleware";
+import {
+  createPost,
+  deletePost,
+  editpost,
+  getAllPost,
+  getPost,
+} from "../controller/postController";
 const router = express.Router();
+
+router.post("/createpost", verifyToken, createPost);
+router.get("/getallpost", verifyToken, getAllPost);
+router.get("/getpost/:id", verifyToken, getPost);
+router.put("/editpost/:id", verifyToken, editpost);
+router.delete("/deletepost/:id", verifyToken, deletePost);
 
 export default router;
